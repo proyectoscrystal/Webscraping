1539d5338a48930d626fdd2f5bcbd46d3c07bb5e
Camilo404 <78864976+Camilo404@users.noreply.github.com>
2021-09-23 17:30:02 -0500

    Se importa el código para obtener el tipo de prenda - Zara

--- src/core/Zara/menCategory.js
+++ src/core/Zara/menCategory.js
@@ -6,19 +6,19 @@ exports.categoriaHombre = async () => {
   const browser = await puppeteer.launch({ headless: false }); //headless true/false para visualizar el navegador
   try {
     const page = await browser.newPage();
+    
+    const prendasHombre = []; //Se crea un array para guardar los productos extraidos
+
+    let count = 5;
 
     //====================CATEGORIAS HOMBRE==========================
-    await page.goto(
-      "https://www.zara.com/co/es/join-life-man-new-in-l3023.html?v1=1886329"
-    ); //Se especifica el enlace para categoría de hombre
+    await page.goto("https://www.zara.com/co/es/join-life-man-new-in-l3023.html?v1=1886329"); //Se especifica el enlace para categoría de hombre
     await page.setViewport({ width: 920, height: 1080 }); //Tamaño de la página
     await page.waitForTimeout(5000); //Tiempo para cargar la página completa
 
-    //Se obtienen los enlaces de las categorías
+    //Se obtienen los enlaces de los tipos de prenda
     const enlaceshombre = await page.evaluate(() => {
-      const elements = document.querySelectorAll(
-        "#sidebar > div > nav > div > ul > li > ul > li > ul > .layout-categories-category--level-3 > a"
-      );
+      const elements = document.querySelectorAll("#sidebar > div > nav > div > ul > li > ul > li > ul > .layout-categories-category--level-3 > a");
 
       const linkshombre = [];
       for (let element of elements) {
@@ -26,8 +26,6 @@ exports.categoriaHombre = async () => {
       }
       return linkshombre;
     });
-
-    const prendasHombre = []; //Se crea un array para guardar los productos extraidos
 
     //Se crea un for el cual llevará todo el proceso
     for (let enlacehombre of enlaceshombre) {
@@ -37,9 +35,7 @@ exports.categoriaHombre = async () => {
 
       //Se obtienen los enlaces de los productos
       const enlacesproductoshombre = await page.evaluate(() => {
-        const elements = document.querySelectorAll(
-          "#main > article > div > section > ul > li > ul > li > div > div > div > a"
-        );
+        const elements = document.querySelectorAll("#main > article > div > section > ul > li > ul > li > div > div > div > a");
 
         const productoshombre = [];
         for (let element of elements) {
@@ -48,14 +44,13 @@ exports.categoriaHombre = async () => {
         return productoshombre;
       });
 
-      //Se crea un for dentro del otro for para entrar a cada enlace de los productos
-      let count = 5;
+      //Se obtiene el nombre del tipo de prenda
+      let nombrecategoria = await page.evaluate(() => document.querySelector("#theme-app > div > div > footer > div > div:nth-child(2) > div > nav > ol > li:nth-child(4) > a > span").textContent);
 
+      //Se crea un for dentro del otro for para entrar a cada enlace de los productos
       for (let enlaceproductohombre of enlacesproductoshombre) {
         try {
-          await page.goto(enlaceproductohombre, {
-            waitUntil: "domcontentloaded",
-          });
+          await page.goto(enlaceproductohombre);
           await autoScroll(page);
 
           //Se evalua cada enlace del cual se extrae el categoria, nombre, precio y caracteristicas
@@ -64,22 +59,12 @@ exports.categoriaHombre = async () => {
 
             const tmp = {};
             tmp.enlaceProducto = currentURL;
-            tmp.categoria = document.querySelector("title").textContent;
-            tmp.imageName = document.querySelector(
-              "#main > article > .product-detail-view__main > div > .product-detail-info > h1"
-            ).textContent;
-            tmp.precio = document.querySelector(
-              ".price__amount > span"
-            ).textContent;
-            tmp.caracteristicas = document.querySelector(
-              "#main > article > div > div > div > .product-detail-description > div > div > div > p"
-            ).textContent;
+            tmp.imageName = document.querySelector("#main > article > .product-detail-view__main > div > .product-detail-info > h1").textContent;
+            tmp.precio = document.querySelector(".price__amount > span").textContent;
+            tmp.caracteristicas = document.querySelector("#main > article > div > div > div > .product-detail-description > div > div > div > p").textContent;
             tmp.caracteristicas = tmp.caracteristicas.split("."); // probando para separar por caracteristicas
             tmp.caracteristicas.pop();
-
-            tmp.enlaceImagen = document.querySelector(
-              "#main > article > div > div > section > ul > li > button > div > div > picture > img"
-            ).src;
+            tmp.enlaceImagen = document.querySelector("#main > article > div > div > section > ul > li > button > div > div > picture > img").src;
             tmp.gender = "Hombre";
             tmp.marca = "Zara";
             tmp.descuento = "";
@@ -90,22 +75,26 @@ exports.categoriaHombre = async () => {
 
             return tmp;
           });
+
           count--;
           prendasHombre.push(prendahombre); //Se guardan las prendas en la constante prendasHombre
           if (count === 0) {
             break;
           }
+
         } catch (error) {
+          //console.error(error.message);
         }
       }
-      getScraping.getscraping(prendasHombre);
-      // console.log(prendasHombre);
-      if (count === 0) {
-        break;
-      }
+
+      prendasHombre.forEach((dato) => {dato.categoria = nombrecategoria});
+
+      //console.log(prendasHombre);
     }
 
+    getScraping.getscraping(prendasHombre);
     //====================CATEGORIAS HOMBRE==========================
+
   } catch (err) {
     console.error(err.message);
   } finally {
--- src/core/Zara/womanCategory.js
+++ src/core/Zara/womanCategory.js
@@ -7,18 +7,18 @@ exports.womanCategory = async () => {
   try {
     const page = await browser.newPage();
 
+    const prendasMujer = []; //Se crea un array para guardar los productos extraidos
+
+    let count = 5;
+
     //====================CATEGORIAS MUJER===========================
-    await page.goto(
-      "https://www.zara.com/co/es/woman-editorial-6-mkt1102.html?v1=1883903"
-    ); //Se especifica el enlace para categoría de mujer
+    await page.goto("https://www.zara.com/co/es/woman-editorial-6-mkt1102.html?v1=1883903"); //Se especifica el enlace para categoría de mujer
     await page.setViewport({ width: 920, height: 1080 }); //Tamaño de la página
     await page.waitForTimeout(5000); //Tiempo para cargar la página completa
 
     //Se obtienen los enlaces de las categorías
     const enlacesmujer = await page.evaluate(() => {
-      const elements = document.querySelectorAll(
-        "#sidebar > div > nav > div > ul > li > ul > li > ul > .layout-categories-category--level-3 > a"
-      );
+      const elements = document.querySelectorAll("#sidebar > div > nav > div > ul > li > ul > li > ul > .layout-categories-category--level-3 > a");
 
       const linksmujer = [];
       for (let element of elements) {
@@ -27,87 +27,74 @@ exports.womanCategory = async () => {
       return linksmujer;
     });
 
-    const prendasMujer = []; //Se crea un array para guardar los productos extraidos
-    let count = 5;
-
     //Se crea un for el cual llevará todo el proceso
     for (let enlacemujer of enlacesmujer) {
-        await page.goto(enlacemujer, { waitUntil: "domcontentloaded" });
-        //await page.waitForTimeout(2000); //Tiempo para cargar la página completa
-        await autoScroll(page); //Función que hace el scroll en la página
+      await page.goto(enlacemujer, { waitUntil: "domcontentloaded" });
+      //await page.waitForTimeout(2000); //Tiempo para cargar la página completa
+      await autoScroll(page); //Función que hace el scroll en la página
 
-        //Se obtienen los enlaces de los productos
-        const enlacesproductosmujer = await page.evaluate(() => {
-          const elements = document.querySelectorAll(
-            "#main > article > div > section > ul > li > ul > li > div > div > div > a"
-          );
+      //Se obtienen los enlaces de los productos
+      const enlacesproductosmujer = await page.evaluate(() => {
+        const elements = document.querySelectorAll('#main > article > .product-groups > section > ul > li > ul > li:nth-child(1) > div > div > a');
 
-          const productosmujer = [];
-          for (let element of elements) {
-            productosmujer.push(element.href);
-          }
-          return productosmujer;
-        });
-
-        //Se crea un for dentro del otro for para entrar a cada enlace de los productos
-        for (let enlaceproductomujer of enlacesproductosmujer) {
-          try {
-            await page.goto(enlaceproductomujer, {
-              waitUntil: "domcontentloaded",
-            });
-            await autoScroll(page);
-
-            //Se evalua cada enlace del cual se extrae el categoria, nombre, precio y caracteristicas
-            const prendamujer = await page.evaluate(() => {
-              const currentURL = window.location.href;
-
-              const tmp = {};
-              tmp.enlaceProducto = currentURL;
-              tmp.categoria = document.querySelector("title").textContent;
-              tmp.imageName = document.querySelector(
-                "#main > article > .product-detail-view__main > div > .product-detail-info > h1"
-              ).textContent;
-              tmp.precio = document.querySelector(
-                ".price__amount > span"
-              ).textContent;
-              tmp.descuento = "";
-              tmp.tag = "";
-              tmp.caracteristicas = document.querySelector(
-                "#main > article > div > div > div > .product-detail-description > div > div > div > p"
-              ).textContent;
-              tmp.caracteristicas = tmp.caracteristicas.split("."); // probando para separar por caracteristicas
-              tmp.caracteristicas.pop();
-
-              tmp.enlaceImagen = document.querySelector(
-                "#main > article > div > div > section > ul > li > button > div > div > picture > img"
-              ).src;
-              tmp.gender = 'Mujer';
-              tmp.marca = 'Zara';
-              tmp.talla = Array.from(document.querySelectorAll('.product-detail-size-selector > div > ul > li > div > div > span'), xTallas => xTallas.textContent);
-              tmp.color = document.querySelector('#main > article > .product-detail-view__main > div > div > p').textContent;
-              tmp.materiales = document.querySelector('#main > article > div.product-detail-view__main > div.product-detail-view__main-content > div > div > div > div > div > div > div:nth-child(6) > span > span').textContent;              
-              
-              return tmp;
-            });
-
-            count--;
-            prendasMujer.push(prendamujer); //Se guardan las prendas en la constante prendasHombre
-            if(count === 0){                
-              break;
-            }
-          } catch (error) {
-            console.log(error);
-          }
+        const productosmujer = [];
+        for (let element of elements) {
+          productosmujer.push(element.href);
         }
-        //console.log(prendasMujer);
-        if(count === 0){
-          break;
+        return productosmujer;
+      });
+
+      //Se obtiene el nombre del tipo de prenda
+      let nombrecategoria = await page.evaluate(() => document.querySelector("#theme-app > div > div > footer > div > div:nth-child(2) > div > nav > ol > li:nth-child(4) > a > span").textContent);
+
+      //Se crea un for dentro del otro for para entrar a cada enlace de los productos
+      for (let enlaceproductomujer of enlacesproductosmujer) {
+        try {
+          await page.goto(enlaceproductomujer);
+          await autoScroll(page);
+
+          //Se evalua cada enlace del cual se extrae el categoria, nombre, precio y caracteristicas
+          const prendamujer = await page.evaluate(() => {
+            const currentURL = window.location.href;
+
+            const tmp = {};
+            tmp.enlaceProducto = currentURL;
+            tmp.imageName = document.querySelector("#main > article > .product-detail-view__main > div > .product-detail-info > h1").textContent;
+            tmp.precio = document.querySelector(".price__amount > span").textContent;
+            tmp.caracteristicas = document.querySelector("#main > article > div > div > div > .product-detail-description > div > div > div > p").textContent;
+            tmp.caracteristicas = tmp.caracteristicas.split("."); // probando para separar por caracteristicas
+            tmp.caracteristicas.pop();
+            tmp.enlaceImagen = document.querySelector("#main > article > div > div > section > ul > li > button > div > div > picture > img").src;
+            tmp.gender = 'Mujer';
+            tmp.marca = 'Zara';
+            tmp.descuento = "";
+            tmp.tag = "";            
+            tmp.talla = Array.from(document.querySelectorAll('.product-detail-size-selector > div > ul > li > div > div > span'), xTallas => xTallas.textContent);
+            tmp.color = document.querySelector('#main > article > .product-detail-view__main > div > div > p').textContent;
+            tmp.materiales = document.querySelector('#main > article > div.product-detail-view__main > div.product-detail-view__main-content > div > div > div > div > div > div > div:nth-child(6) > span > span').textContent;              
+            
+            return tmp;
+          });
+
+          count--;
+          prendasMujer.push(prendamujer); //Se guardan las prendas en la constante prendasMujer
+          if(count === 0){                
+            break;
+          }
+
+        } catch (error) {
+          //console.error(error.message);
         }
-        
+      }
+
+      prendasMujer.forEach((dato) => {dato.categoria = nombrecategoria});
+
+      //console.log(prendasMujer);
     }
-    getscraping.getscraping(prendasMujer);
 
+    getscraping.getscraping(prendasMujer);
     //====================CATEGORIAS MUJER===========================
+
   } catch (err) {
     console.error(err.message);
   } finally {
