02d8a3620d969c6f93988c5beb1fcb9f85b53f69
juanDev-47 <juanp.arenas@udea.edu.co>
2021-10-04 09:41:28 -0500

    cambios en el scrapingCtl

--- src/core/scrapingCtl.js
+++ src/core/scrapingCtl.js
@@ -9,7 +9,12 @@ exports.averagePrice = async (req, res) => {
     // ver que valores llegan desde el front
     // console.log(filtro);
 
-    arr = await Business.find(filtro,{"base64":1,"precio":1, "descuento": 1, "imageName": 1, "origin":1, "color":1, "categoria":1,"caracteristicas":1, "use":1,"estado":1, "createdAt":1, "talla":1, "numeroTallas":1, "tipoPrenda": 1});
+    try {
+        arr = await Business.find(filtro,{"base64":1,"precio":1, "descuento": 1, "imageName": 1, "origin":1, "color":1, "categoria":1,"caracteristicas":1, "use":1,"estado":1, "createdAt":1, "talla":1, "numeroTallas":1, "tipoPrenda": 1});
+    } catch (error) {
+        console.log("no se obtuvo respuesta");
+        return res.json({mensaje: "no hubo respuesta para los criterios de busqueda"});
+    }
 
     
     // respuesta para el frontend
